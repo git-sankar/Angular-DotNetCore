bd3360c86e91bac2a7aaf87347de945115ae1ffa
git-sankar <Knas_2004@yahoo.co.in>
2024-02-12 00:09:20 +0000

    Repository Pattern Draft

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -1,4 +1,5 @@
 using API.Entites;
+using Core.Interfaces;
 using Infrastructure.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -9,23 +10,24 @@ namespace API.Controllers
     [Route("api/[controller]")]
     public class ProductsController : ControllerBase
     {
-        public readonly StoreContext _context;
-        public ProductsController(StoreContext context)
+        public readonly IProductRepository _productRepository;
+        
+        public ProductsController(IProductRepository productRepository)
         {
-            _context = context;
+            _productRepository = productRepository;
         }
         
         [HttpGet]
         public async Task<ActionResult<List<Product>>> GetProducts()
         {
-            var products = await _context.Products.ToListAsync();
+            var products = await _productRepository.GetProductsAsync();
 
-            return products;
+            return Ok(products);
         }
         [HttpGet("{id}")]
         public async Task<ActionResult<Product>> GetProducts(int id)
         {
-            return await _context.Products.FindAsync(id);
+            return await _productRepository.GetProductByIdAsync(id);
         }
         
     }
